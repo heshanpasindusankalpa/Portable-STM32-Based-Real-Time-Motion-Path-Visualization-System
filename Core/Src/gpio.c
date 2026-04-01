a9d130989a2a829ffd73b33d3a3920ed1e49dcf2
heshanpasindusankalpa <pasindusankalpa2021@gmail.com>
2026-04-01 18:51:01 +0530

    Initial STM32 project setup

---
+++ Core/Src/gpio.c
@@ -0,0 +1,23 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
